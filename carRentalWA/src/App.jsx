52bcd6a77af7a2b8444914804fc2e097977ca44d
Anova <119845896+Anova-12@users.noreply.github.com>
2024-03-14 16:43:38 +0545

    LALALALA

---
+++ carRentalWA/src/App.jsx
@@ -0,0 +1,14 @@
+import Navigation from './components/Navigation'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Navigation></Navigation>
+    </>
+  )
+}
+
+export default App
---
+++ carRentalWA/src/components/Navigation.jsx
@@ -0,0 +1,30 @@
+import '../compCss/Navigation.css';
+
+function Navigation(){
+    
+
+    return(
+        <div className="navi">
+
+
+            <div className="logo">
+                <img src="" alt="LOGO" />
+            </div>
+
+
+            <div className="navs">
+            <nav>
+                <ul>
+                    <li>home</li>
+                    <li>contact</li>
+                </ul>
+            </nav>
+            </div>
+
+
+        </div>
+        
+    );
+}
+
+export default Navigation;
